1037095012ca6a72e742f401a741571968bf5bbe
Bryce Sheridan <91303219+BryceSh@users.noreply.github.com>
2022-10-20 19:45:58 +0000

    updated

--- .ccls-cache/@home@runner@UpperLowercase-Lab/main.c
+++ .ccls-cache/@home@runner@UpperLowercase-Lab/main.c
@@ -13,28 +13,26 @@ char convert(char in) {
 
 int main(int argc, char **argu) {
   if (argc < 3) {
-    printf("Invalid Usage! ./ {source} {destination}\n");
     return -1;
-  }
-  int allowedBytes = 16;
+    }
   int fd = open(argu[1], O_RDONLY, 0);
   int out = open(argu[2], O_WRONLY | O_CREAT, 0777);
   if (fd > 0) {
-    char buffer[allowedBytes];
-    int count = allowedBytes;
-    while (count == allowedBytes) {
-      count = read(fd, buffer, allowedBytes);
+    char buffer[16];
+    int count = 16;
+    while (count == 16) {
+      count = read(fd, buffer, 16);
       int i;
       for (i = 0; i<count; i++) {
         buffer[i] = convert(buffer[i]);
       }
       int x = write(out, buffer, count);
       if (x == -1) {
-        printf("Something happened writing the file!\n");
+        perror("Something happened writing the file!\n");
       }
     }
   } else {
-    printf("Invalid source file! Please try again!\n");
+    perror("Invalid file\n");
     return -1;
   }
   return 0;
